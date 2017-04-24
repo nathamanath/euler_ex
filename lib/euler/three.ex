7f772fd4701d1954e8c64d039f854604744e6563
NathanG <nathangoddard@me.com>
2017-04-24 21:07:37 +0100

    optimise 3

--- lib/euler/three.ex
+++ lib/euler/three.ex
@@ -4,46 +4,46 @@ defmodule Euler.Three do
   What is the largest prime factor of the number 600851475143?
   """
 
-  import Euler.Utils, only: [is_prime?: 2]
-
   @doc """
   get highest prime factor of `n`
   """
-  def run(n) do
-    # start on 3 as 2 is not the answer and makes update to i slightly easier
-    three(n, :math.sqrt(n), 3, [2], 0)
-  end
+  def run(n), do: top_factor(n, 2, [])
 
   @doc """
-  `n` - subject
-  `rt_n` - sqrt(n)
-  `i` - current integer
-  `primes` - list of found primes in descending order
-  `last_factor` - hoghest known prime factor of `n`
+  get prime factors of `n`
 
-  each int < sqrt n
-  check int is prime
-  if so check int is factor of n
-  keep last found factor
+  `n` - number to find factors of
+  `i` - iterator
+  `factors` - list of found factors
+
+  returns List
+
+  if `i` is a factor of `n`
+
+  next prime factor goes into `n / i`
+
+  once `n` == 1, we have the top factor
+
+  `i` is always prime when `rem(n, i) == 0`
+  `n` is not prime until `n == i`
+
+  600851475143 / 71 = 8462696833.0
+  8462696833 / 839 = 10086647.0
+  10086647 / 1471 = 6857.0
+  6857 / 6857 = 1.0
+
+  https://en.wikipedia.org/wiki/Integer_factorization
+
+  brute force aproach ran in ~30s, this way takes ~0.5s
   """
-  def three(_n, rt_n, i, _primes, last_factor) when i >= rt_n, do: last_factor
-
-  def three(n, rt_n, i, primes, last_factor) do
-    new_i = i + 2
-
-    # PLaying with with
-    # TODO: refactor
-    with true <- is_prime?(i, primes),
-      true <- is_factor(n, i) do
-        three(n, rt_n, new_i, [i | primes], i)
-    else
-      :not_factor -> three(n, rt_n, new_i, [i | primes], last_factor)
-      _ -> three(n, rt_n, new_i, primes, last_factor)
-    end
-
+  def top_factor(1, _i, [top | _rest]), do: top
+  def top_factor(n, i, factors) when rem(n, i) == 0 do
+    top_factor(div(n, i), i, [i | factors])
   end
 
-  defp is_factor(n, i) when rem(n, i) == 0, do: true
-  defp is_factor(_n, _i), do: :not_factor
+  # skip all values of i which are not factors of n
+  # 2 and odds as 2 goes into all evens
+  def top_factor(n, 2, factors), do: top_factor(n, 3, factors)
+  def top_factor(n, i, factors), do: top_factor(n, i + 2, factors)
 
 end
