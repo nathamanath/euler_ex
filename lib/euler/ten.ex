25c04b031f3a0c11fc60bae9f25194ba87157c16
NathanG <nathangoddard@me.com>
2017-04-28 15:42:20 +0100

    ten with crap sive

--- lib/euler/ten.ex
+++ lib/euler/ten.ex
@@ -5,25 +5,49 @@ defmodule Euler.Ten do
   Find the sum of all the primes below two million.
   """
 
-  # OPTIMIZE: works but is too slow
-  # http://www.bytehood.com/sieve-of-eratosthenes-finding-all-primes-below-a-limit-n/47/
-  #
-  # * Create list of all numbers from 2 to N.
-  # * Let p initially equal to 2. The first prime.
-  # * Strike all multiples of p from the list less than or equal to N.
-  # * Find the next number after p, that’s not yet crossed out on the list. This is the next prime.
-  # * Repeat step 3 and 4 for the new p, unless p2 is greater than N.
-  # * Numbers not crossed out on the list are primes below the limit N.
+  @doc """
+  Get sum of prime numbers < `n`
 
-  def run(n), do: ten(n, 2, [])
+  ## Examples
 
-  def ten(n, n, primes), do: Enum.reduce(primes, fn(n, acc) -> n + acc end)
+    iex> Euler.Ten.run(10)
+    17
 
-  def ten(n, i, primes) do
-    case is_prime?(i, primes) do
-      true -> ten(n, i+1, [i | primes])
-      _ -> ten(n, i+1, primes)
-    end
+    iex> Euler.Ten.run(2_000_000)
+    142913828922
+
+  """
+  @spec run(number) :: integer
+  def run(number), do: number |> sive |> Enum.reduce(fn(i, acc) -> i + acc end)
+
+  @doc """
+  Eratosthenes sive algorithm - Find prime numbers under `number`
+
+  * Create list of all numbers from 2 to N.
+  * Let p initially equal to 2. The first prime.
+  * Strike all multiples of p from the list less than or equal to N.
+  * Find the next number after p, that’s not yet crossed out on the list. This
+  ..is the next prime.
+  * Repeat step 3 and 4 for the new p, unless p2 is greater than N.
+  * Numbers not crossed out on the list are primes below the limit N.
+
+  OPTIMIZE: too slow... try not to make a list each time
+
+  ## Examples
+
+    iex> Euler.Ten.sive(10)
+    [7, 5, 3, 2]
+
+  """
+  @spec sive(number) :: list
+  def sive(number) when is_integer(number), do: do_sive(Enum.to_list(2..number), [])
+
+  # list is working list... initially integers from 2..n
+  # primes is list of found prime numbers
+  defp do_sive([], primes), do: primes
+
+  defp do_sive([prime | rest], primes) do
+    do_sive(Enum.filter(rest, fn(x) -> rem(x, prime) != 0 end), [prime | primes])
   end
 
 end
--- test/euler/ten_test.exs
+++ test/euler/ten_test.exs
@@ -1,14 +1,4 @@
 defmodule Euler.TenTest do
   use ExUnit.Case
-
-  alias Euler.Ten
-
-  test "sum of the primes below 10" do
-    assert Ten.run(10) == 17
-  end
-
-  test "sum of the primes below 2_000_000" do
-    assert Ten.run(2_000_000) == 142913828922
-  end
-
+  doctest Euler.Ten
 end
