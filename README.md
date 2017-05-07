6541ddceea01dbd3478e79d20b0e54ff1dbec40e
NathanG <nathangoddard@me.com>
2017-05-07 22:37:36 +0100

    both solutions to 15

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 [Project Euler](https://projecteuler.net) solutions in [Elixir](http://elixir-lang.org/).
 for learning and for fun.
+
+
+## Notes
+
+Integer division: use `kernel.div/2`
--- lib/euler/q_015.ex
+++ lib/euler/q_015.ex
@@ -14,74 +14,111 @@ defmodule Euler.Q015 do
   """
 
   @doc """
-  Count possible routes across of `n` by `n` grid moving right or down only.
+  Maths solution: http://mathforum.org/advanced/robertd/manhattan.html
 
-  * start at point [{0, 0}]
-  * make all possible moves ([{0, 0}, {0, 1}], [{0, 0}, {1, 0}])
-  * possible moves are x+1, or y+1, x and y must be < n
-  * count routs to {n, n}
+  If you view routes as a tree, you get this:
 
-  it seems that: number of routes to cell is sum of adjacent cells routes
+            1
+          1   1
+        1   2   1
+     1    3   3   1
+  1    4    6   4   1
 
-  OPTIMIZE: http://mathforum.org/advanced/robertd/manhattan.html
+  The number of paths are the central binomial coefficients. Central meaning they
+  fall along the center line of Pascal's triangle
+
+  and because maths, `(2n)! / n!^2` gives us central binomial coefficient for an
+  nxn grid.
+
+  Problem with this approach is it dosent scale for large values of n... n==100
+  for example causes an integer overflow. My less smart solution below can handle
+  this.
 
   ## Examples
 
-    iex> Euler.Q015.run 2, 2
+    iex> Euler.Q015.run 2
     6
 
-    iex> Euler.Q015.run 20, 20
-    137846528820
+  """
+  @spec run(integer) :: integer
+  def run(n), do: round primorial(2*n) / :math.pow(primorial(n), 2)
+
+  # primorial: product of numbers 1..n
+  @spec primorial(integer) :: integer
+  defp primorial(n), do: Enum.reduce(1..n, 1, &(&1 * &2))
+
+  @doc """
+  My initial solution. Works for larger values of n, but takes longer about it.
+
+  Route to each point is sum of point to left and point above
+  eg: (n = 2)
+
+  1 1 1
+  1 2 3
+  1 3 6
+
+  1, Generate grid of routes for nxn
+  2, get number in bottom right
+
+  ## Examples
+
+    iex> Euler.Q015.run_initial 2
+    6
+
+    # cant do this with the other method
+    iex> Euler.Q015.run_initial 100
+    90548514656103281165404177077484163874504589675413336841320
 
   """
-  @spec run(integer, integer) :: integer
-  def run(w, h) do
-    all_routes(w, h)
-    |> Enum.at(h)
-    |> Enum.at(w)
-  end
-
-  def all_routes(w, h) do
-    do_all_routes(w, h, 0, [])
-  end
-
-  defp do_all_routes(_w, h, row, routes) when row > h do
-    Enum.reverse routes
-  end
-
-  defp do_all_routes(w, h, row, []) do
-    do_all_routes(w, h, row+1, [row_routes(w, [], [])])
-  end
-
-  defp do_all_routes(w, h, row, [previous | _rest] = routes) do
-    do_all_routes(w, h, row+1, [row_routes(w, previous, []) | routes])
+  @spec run_initial(integer) :: integer
+  def run_initial(n) do
+    grid_routes(n)
+    |> Enum.at(n)
+    |> Enum.at(n)
   end
 
   @doc """
-  get routes to each point in row
-
   ## Examples
 
-    iex> Euler.Q015.row_routes 2, [], []
-    [1, 1, 1]
+    # iex> Euler.Q015.grid_routes 2
+    # [[1, 1, 1], [1, 2, 3], [1, 3, 6]]
 
-    iex> Euler.Q015.row_routes 2, [1, 1, 1], []
-    [1, 2, 3]
+    iex> Euler.Q015.grid_routes 4
+    [[1, 1, 1, 1, 1], [1, 2, 3, 4, 5], [1, 3, 6, 10, 15], [1, 4, 10, 20, 35], [1, 5, 15, 35, 70]]
 
-    iex> Euler.Q015.row_routes 2, [1, 2, 3], []
-    [1, 3, 6]
-
+    # iex> Euler.Q015.grid_routes 8
+    # [[1, 1, 1], [1, 2, 3], [1, 3, 6]]
   """
-  @spec row_routes(integer, [integer], [integer]) :: [[integer]]
-  def row_routes(w, _previous, row) when length(row) > w, do: Enum.reverse row
+  def grid_routes(n), do: do_grid_routes(n, 0, 0, [])
 
-  # start of row... always 1
-  def row_routes(w, previous, []), do: row_routes(w, previous, [1])
-
-  # work out others based on previous row and previous member of current row
-  def row_routes(w, previous, [last | _] = row) do
-    above = Enum.at(previous, length(row), 0)
-    row_routes(w, previous, [last + above | row])
+  # end of last col
+  defp do_grid_routes(n, row, _col, rows) when row > n do
+    rows
+    |> Enum.reverse
+    |> Enum.chunk(n+1)
   end
+
+  # end of row
+  defp do_grid_routes(n, row, col, rows) when col > n do
+    do_grid_routes(n, row + 1, 0, rows)
+  end
+
+  # row 0 any column
+  defp do_grid_routes(n, 0, col, rows), do: do_grid_routes(n, 0, col + 1, [1 | rows])
+
+  # col 0 any row
+  defp do_grid_routes(n, row, 0, rows), do: do_grid_routes(n, row, 1, [1 | rows])
+
+  defp do_grid_routes(n, row, col, [last | _] = rows) do
+    # reconstruct matrix from rows vector
+    above = rows
+      |> Enum.reverse
+      |> Enum.chunk(n+1)
+      |> Enum.at(row-1)
+      |> Enum.at(col)
+
+    do_grid_routes(n, row, col + 1, [above + last | rows])
+  end
+
 
 end
