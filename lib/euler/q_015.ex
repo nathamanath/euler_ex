9801bde770de7739989745a9d3ac49a03f2dea9b
NathanG <nathangoddard@me.com>
2017-05-07 12:30:39 +0100

    start 15

--- lib/euler/q_015.ex
+++ lib/euler/q_015.ex
@@ -14,11 +14,31 @@ defmodule Euler.Q015 do
   """
 
   @doc """
-  Find possible routes out of n x n grid
+  Count possible routes across of `n` by `n` grid moving right or down only.
+
+  * start at point [{0, 0}]
+  * make all possible moves ([{0, 0}, {0, 1}], [{0, 0}, {1, 0}])
+  * possible moves are x+1, or y+1, x and y must be < n
+  * count routs to {n, n}
+
+  ## Examples
+
+    iex> Euler.Q015.run 2
+    6
+
   """
   @spec run(integer) :: integer
   def run(n) do
-    1
+    step(n, [{0, 0}])
+  end
+
+  @doc """
+  recurse around an `n` by `n` grid, finding routes from {0, 0} to {n, n}
+  """
+  @spec step(integer, [{integer, integer}]) :: integer
+  def step(n, [{n, n} | _rest]), do: 1
+  def step(n, [{x, y} | _rest] = steps) do
+    step(n, [{x+1, y+1} | steps])
   end
 
 end
