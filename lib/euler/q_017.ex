76269f18affbafe5e930ab8cd9ff26aa77686efd
NathanG <nathan@nathansplace.co.uk>
2017-05-10 22:46:15 +0100

    17 start

---
+++ lib/euler/q_017.ex
@@ -0,0 +1,28 @@
+defmodule Euler.Q017 do
+  @moduledoc """
+  If the numbers 1 to 5 are written out in words: one, two, three, four, five,
+  then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+  If all the numbers from 1 to 1000 (one thousand) inclusive were written out in
+  words, how many letters would be used?
+
+  NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and
+  forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20
+  letters. The use of "and" when writing out numbers is in compliance with
+  British usage.
+  """
+
+  @doc """
+  Sum of the length of numbers from `range` in words
+
+  ## Examples
+
+    iex> Euler.Q017.run 1..5
+    19
+
+  """
+  def run(range) do
+
+  end
+
+end
---
+++ test/euler/q_017_test.exs
@@ -0,0 +1,5 @@
+defmodule Euler.Q017Test do
+  use ExUnit.Case
+
+  doctest Euler.Q017
+end
