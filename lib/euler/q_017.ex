e9faf67c6ec7ccdea72996b2300bc51824e22ffb
NathanG <nathangoddard@me.com>
2017-05-13 22:42:03 +0100

    question 17

--- lib/euler/q_017.ex
+++ lib/euler/q_017.ex
@@ -12,7 +12,134 @@ defmodule Euler.Q017 do
   British usage.
   """
 
-  @number_map [{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}]
+  defmodule Num do
+    @moduledoc """
+    For turning numbers into words
+    """
+    # Map numbers to strings
+    @numbers %{
+      1 => %{
+        name: "one"
+      },
+      2 => %{
+        name: "two",
+        suffixed: "twen"
+      },
+      3 => %{
+        name: "three",
+        suffixed: "thir" # thirty thirteen
+      },
+      4 => %{
+        name: "four",
+        ty: "for"
+      },
+      5 => %{
+        name: "five",
+        suffixed: "fif"
+      },
+      6 => %{
+        name: "six"
+      },
+      7 => %{
+        name: "seven"
+      },
+      8 => %{
+        name: "eight",
+        suffixed: "eigh"
+      },
+      9 => %{
+        name: "nine"
+      }
+    }
+
+    @doc """
+    Convert integer to words
+
+    ## Examples
+      iex> Euler.Q017.Num.as_words(5)
+      "five"
+
+    """
+    @spec as_words(integer) :: String.t
+    def as_words(i), do: do_as_words(get_units(i), [])
+
+    @spec do_as_words([integer], [String.t]) :: String.t
+    defp do_as_words([0, 0, 0, 0], parsed) do
+      parsed
+      |> Enum.reverse()
+      |> Enum.join(" ")
+    end
+
+    defp do_as_words([0, 0, 0, units], parsed) do
+      do_as_words([0, 0, 0, 0], [@numbers[units][:name] | parsed])
+    end
+
+    # teens
+    # First the special cases... thank you English
+    defp do_as_words([0, 0, 1, 0], parsed), do: do_as_words([0, 0, 0, 0], ["ten" | parsed])
+    defp do_as_words([0, 0, 1, 1], parsed), do: do_as_words([0, 0, 0, 0], ["eleven" | parsed])
+    defp do_as_words([0, 0, 1, 2], parsed), do: do_as_words([0, 0, 0, 0], ["twelve" | parsed])
+
+    defp do_as_words([0, 0, 1, units], parsed) do
+      do_as_words([0, 0, 0, 0], ["#{as_prefix(units, :teen)}teen" | parsed])
+    end
+
+    # tens > 1
+    defp do_as_words([0, 0, tens, units], parsed) do
+      do_as_words([0, 0, 0, units], ["#{as_prefix(tens, :ty)}ty" | parsed])
+    end
+
+    # hundreds
+    defp do_as_words([0, hundreds, 0, 0], parsed) do
+      do_as_words([0, 0, 0, 0], ["#{as_words(hundreds)} hundred" | parsed])
+    end
+
+    defp do_as_words([0, hundreds, tens, units], parsed) do
+      do_as_words([0, 0, tens, units], ["and", "#{as_words(hundreds)} hundred" | parsed])
+    end
+
+    # thousands
+    defp do_as_words([thousands, hundreds, tens, units], parsed) do
+      do_as_words([0, hundreds, tens, units], ["#{as_words(thousands)} thousand" | parsed])
+    end
+
+    @doc """
+    Get number as a prefix to a unit suffix
+
+    ## Examples
+
+      iex> Euler.Q017.Num.as_prefix(3, 10)
+      "thir"
+
+      iex> Euler.Q017.Num.as_prefix(6, 10)
+      "six"
+    """
+    @spec as_prefix(integer, integer) :: String.t
+    def as_prefix(4, :ty), do: @numbers[4][:ty]
+
+    def as_prefix(i, _context) do
+      @numbers[i][:suffixed] || @numbers[i][:name]
+    end
+
+    @doc """
+    break integer < one million into units
+
+    ## Examples
+
+      iex> Euler.Q017.Num.get_units(1111)
+      [1, 1, 1, 1]
+
+    """
+    @spec get_units(integer) :: [integer]
+    def get_units(i), do: do_get_units(i, 1000, [])
+
+    @spec do_get_units(integer, integer, [integer]) :: [integer]
+    defp do_get_units(i, 1, units), do: [i | units] |> Enum.reverse
+
+    defp do_get_units(i, divisor, units) do
+      do_get_units(rem(i, divisor), div(divisor, 10), [div(i, divisor) | units])
+    end
+  end
 
   @doc """
   Sum of the length of numbers from `range` in words
@@ -22,21 +149,14 @@ defmodule Euler.Q017 do
     iex> Euler.Q017.run 1..5
     19
 
-    iex> Euler.Q017.run 1..1000
-    19
-
   """
+  @spec run(Range.t) :: integer
   def run(range) do
     range
-    |> Enum.map(&number_to_words/1)
+    |> Enum.map(&Num.as_words/1)
+    |> Enum.map(&String.replace(&1, " ", ""))
     |> Enum.map(&String.length/1)
     |> Enum.reduce(0, &(&1 + &2))
-  end
-
-  # TODO: numbers > 5
-  defp number_to_words(number) do
-    {_key, word} = @number_map |> Enum.find(fn({k, v}) -> k == number end)
-    word
   end
 
 end
--- test/euler/q_017_test.exs
+++ test/euler/q_017_test.exs
@@ -1,5 +1,40 @@
 defmodule Euler.Q017Test do
   use ExUnit.Case
 
-  doctest Euler.Q017
+  alias Euler.Q017
+  alias Euler.Q017.Num
+
+  doctest Q017
+
+  test "char count in range of numbers" do
+    assert Q017.run(10..19) == 70
+    assert Q017.run(20..99) == 748
+    assert Q017.run(100..999) == 20_259
+  end
+
+  defmodule NumTest do
+    use ExUnit.Case
+
+    alias Euler.Q017.Num
+
+    doctest Num
+
+    test "number to words" do
+      assert Num.as_words(10) == "ten"
+
+      assert Num.as_words(20) == "twenty"
+      assert Num.as_words(25) == "twenty five"
+
+      assert Num.as_words(100) == "one hundred"
+      assert Num.as_words(110) == "one hundred and ten"
+
+      assert Num.as_words(1000) == "one thousand"
+      assert Num.as_words(1100) == "one thousand one hundred"
+
+      assert Num.as_words(4) == "four"
+      assert Num.as_words(14) == "fourteen"
+      assert Num.as_words(40) == "forty"
+    end
+
+  end
 end
